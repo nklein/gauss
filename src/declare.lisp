79eba9eaef67553a824484cac7bbc199ba47877d
Patrick Stein <pat@nklein.com>
2016-01-26 11:11:57 -0600

    Broke up solver into more coherent bits.

--- src/declare.lisp
+++ src/declare.lisp
@@ -6,8 +6,10 @@
   (template:define-template instantiate-matrix-type (type)
     `(progn
        (template:instantiate-templated-function mref ,type)
+       (template:instantiate-templated-function set-mref ,type)
        (template:instantiate-templated-function vref ,type)
        (template:instantiate-templated-function vtref ,type)
+       (template:instantiate-templated-function set-vref ,type)
        (template:instantiate-templated-function make-matrix ,type)
        (template:instantiate-templated-function make-matrix* ,type)
        (template:instantiate-templated-function make-vector ,type)
@@ -16,9 +18,7 @@
        (template:instantiate-templated-function m+ ,type ,type)
        (template:instantiate-templated-function v+ ,type ,type)
        (template:instantiate-templated-function m* ,type ,type)
-       (template:instantiate-templated-function solve-by-gaussian-elimination
-                                                ,type)
-       (template:instantiate-templated-function solve ,type ,type)
+       (template:instantiate-template make-solver ,type ,type)
        ',type))
 
   (defmacro define-matrix-type (type)
--- src/solve.lisp
+++ src/solve.lisp
@@ -2,76 +2,72 @@
 
 (in-package #:gauss)
 
-(template:define-templated-function solve-by-gaussian-elimination
+(template:define-templated-function find-pivot-row (type) (m col)
+  `(loop :with best-pivot-row fixnum := col
+      :with best-pivot-val ,type := (mref '(,type) m col col)
+      :for row fixnum :from (1+ best-pivot-row) :below (mrows m)
+      :for val ,type := (mref '(,type) m row col)
+      :when (< best-pivot-val val)
+        :do (setf best-pivot-row row
+                  best-pivot-val val)
+      :finally (return best-pivot-row)))
+
+(template:define-templated-function swap-rows-of-matrix (type) (m row-a row-b)
+  `(loop :for col fixnum :below (mcols m)
+      :for a ,type := (mref '(,type) m row-a col)
+      :for b ,type := (mref '(,type) m row-b col)
+      :do (set-mref '(,type) b m row-a col)
+      :do (set-mref '(,type) a m row-b col)))
+
+(template:define-templated-function row-operation (type)
+    (m row row-factor other-row other-row-factor &optional (start 0))
+  "Reduce row ROW by taking each element in the row and replacing it
+with the element times ROW-FACTOR plus the corresponding element of
+the OTHER-ROW times the OTHER-ROW-FACTOR.  Start with index START."
+  `(loop :for col :from start :below (mcols m)
+      :for r ,type := (mref '(,type) m row col)
+      :for o ,type := (mref '(,type) m other-row col)
+      :for v ,type := (+ (* r row-factor)
+                         (* o other-row-factor))
+      :do (set-mref '(,type) v m row col)))
+
+(template:define-templated-function eliminate-column (type) (m v col)
+  `(loop :with pivot-row fixnum := col
+      :with pivot-value ,type := (- (mref '(,type) m pivot-row col))
+      :for row fixnum :from (1+ col) :below (mcols m)
+      :for value ,type := (mref '(,type) m row col)
+      :do (row-operation '(,type)
+                         m row pivot-value pivot-row value col)
+      :do (row-operation '(,type)
+                         v row pivot-value pivot-row value)))
+
+(template:define-templated-function forward-propagation (type) (m v)
+  `(loop :for col :below (mcols m)
+      :for pivot-row := (find-pivot-row '(,type) m col)
+      :unless (= pivot-row col)
+      :do (progn
+            (swap-rows-of-matrix '(,type) m pivot-row col)
+            (swap-rows-of-matrix '(,type) v pivot-row col))
+      :do (eliminate-column '(,type) m v col)))
+
+(template:define-templated-function back-substitute (type) (m v)
+  `(loop :for row fixnum :from (1- (mrows m)) :downto 0
+      :for pivot ,type := (mref '(,type) m row row)
+      :for val ,type :=  (if (zerop pivot)
+                             pivot
+                             (/ (- (vref '(,type) v row)
+                                   (loop :for c :from (1+ row) :below (mcols m)
+                                      :summing (the ,type
+                                                    (* (mref '(,type) m row c)
+                                                       (vref '(,type) v c)))))
+                                pivot))
+      :do (set-vref '(,type) val v row)))
+
+(template:define-templated-function solve-by-forward-sub-back-prop
     (type) (m v)
-  `(flet ((find-pivot-row (col)
-            (loop :with best-pivot-row fixnum := col
-               :with best-pivot-val ,type := (mref '(,type) m col col)
-               :for row fixnum :from (1+ best-pivot-row) :below (mrows m)
-               :for val ,type := (mref '(,type) m row col)
-               :when (< best-pivot-val val)
-                 :do (setf best-pivot-row row
-                           best-pivot-val val)
-               :finally (return best-pivot-row)))
-
-          (swap-rows-of-matrix (row-a row-b)
-            (let ((vals (mvals m))
-                  (mrf (mrf m))
-                  (mcf (mcf m)))
-              (declare (type (array ,type (*)) vals)
-                       (type fixnum mrf mcf))
-              (loop :with mrf-a fixnum := (* mrf row-a)
-                    :with mrf-b fixnum := (* mrf row-b)
-                 :for col :below (mcols m)
-                 :for a := (+ mrf-a (* mcf col))
-                 :for b := (+ mrf-b (* mcf col))
-                 :do (rotatef (aref vals a) (aref vals b)))))
-
-          (swap-rows-of-vector (row-a row-b)
-            (let ((vals (mvals v)))
-              (declare (type (array ,type) vals))
-              (rotatef (aref vals row-a) (aref vals row-b))))
-
-          (eliminate-column (col)
-            (let ((p (- (mref '(,type) m col col)))
-                  (vals (mvals m))
-                  (vvals (mvals v))
-                  (mrf (mrf m))
-                  (mcf (mcf m)))
-              (loop :for row :from (1+ col) :below (mcols m)
-                 :for q := (mref '(,type) m row col)
-                 :unless (zerop q)
-                 :do (progn
-                       (loop :for c :from col :below (mcols m)
-                          :for p* := (mref '(,type) m col c)
-                          :for q* := (mref '(,type) m row c)
-                          :for mindex := (+ (* mrf row) (* mcf c))
-                          :do (setf (aref vals mindex) (+ (* p q*) (* p* q))))
-                       (setf (aref vvals row) (+ (* p (aref vvals row))
-                                                 (* (aref vvals col) q)))))))
-
-          (back-substitute ()
-            (let ((vvals (mvals v)))
-              (declare (type (array ,type (*)) vvals))
-              (loop :for col fixnum :from (1- (mrows m)) :downto 0
-                 :for pivot ,type := (mref '(,type) m col col)
-                 :unless (zerop pivot)
-                 :do (setf (aref vvals col)
-                           (/ (- (aref vvals col)
-                                 (loop :for c :from (1+ col) :below (mcols m)
-                                    :summing (the ,type
-                                                  (* (mref '(,type) m col c)
-                                                     (aref vvals c)))))
-                              pivot))))))
-
-     (loop :for col :below (mcols m)
-        :for pivot-row := (find-pivot-row col)
-        :unless (= pivot-row col)
-        :do (progn
-              (swap-rows-of-matrix pivot-row col)
-              (swap-rows-of-vector pivot-row col))
-        :do (eliminate-column col))
-     (back-substitute)
+  `(progn
+     (forward-propagation '(,type) m v)
+     (back-substitute '(,type) m v)
      (values v m)))
 
 (template:define-templated-function solve (type-m type-v) (m v)
@@ -81,4 +77,17 @@
           (assertion (= (mrows m) (mcols m))))
        (let ((m (copy-matrix m ',new-type-v))
              (v (copy-matrix v ',new-type-v)))
-         (solve-by-gaussian-elimination '(,new-type-v) m v)))))
+         (values (solve-by-forward-sub-back-prop '(,new-type-v) m v))))))
+
+(template:define-template make-solver (type-m type-v)
+  (let ((type (contagion-type type-m type-v)))
+    `(progn
+       (template:instantiate-templated-function find-pivot-row ,type)
+       (template:instantiate-templated-function swap-rows-of-matrix ,type)
+       (template:instantiate-templated-function row-operation ,type)
+       (template:instantiate-templated-function eliminate-column ,type)
+       (template:instantiate-templated-function forward-propagation ,type)
+       (template:instantiate-templated-function back-substitute ,type)
+       (template:instantiate-templated-function solve-by-forward-sub-back-prop
+                                                ,type)
+       (template:instantiate-templated-function solve ,type-m ,type-v))))
